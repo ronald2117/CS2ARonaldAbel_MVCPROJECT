0aa8e2a952af74af7fc6f27a2744e2bc25ac3cc8
PL25 <PL25@PL25>
2025-03-21 19:19:03 -0700

    initial

---
+++ CS2ARonaldAbel_MVCPROJECT/wwwroot/js/Ajax.js
@@ -0,0 +1,14 @@
+﻿const AjaxGET = async (url) => {
+    return await $.ajax({
+        url: url,
+        type: "Get",
+    });
+}
+
+const AjaxPOST = async (url, data) => {
+    return await $.ajax({
+        url: url,
+        type: "Post",
+        data: data
+    });
+}
